5aab2158f9ffb41924a1781b1fd8a3957efa5e16
Dima Samoilik <103739636+dimadimaya@users.noreply.github.com>
2023-04-12 17:21:13 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authReducer";
 
-const rootReducer = {
-  [authSlice.name]: authSlice.reducer,
-};
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: authSlice.reducer,
+  },
 });
